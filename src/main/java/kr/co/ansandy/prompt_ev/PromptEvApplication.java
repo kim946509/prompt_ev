6da6a36089b1f679e68122a5201535ddbb980fd4
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-12-11 16:20:06 +0900

    Initial commit

---
+++ src/main/java/kr/co/ansandy/prompt_ev/PromptEvApplication.java
@@ -0,0 +1,13 @@
+package kr.co.ansandy.prompt_ev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromptEvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromptEvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/ansandy/prompt_ev/PromptEvApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.ansandy.prompt_ev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromptEvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

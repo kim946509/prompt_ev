85e90c940b79e689942703e431d7b05efee63b49
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-12-13 01:21:01 +0900

    fix : BaseEntity 날짜 컬럼 타입 수정

--- src/main/java/kr/co/ansandy/prompt_ev/entity/BaseEntity.java
+++ src/main/java/kr/co/ansandy/prompt_ev/entity/BaseEntity.java
@@ -16,10 +16,10 @@ import java.time.LocalDateTime;
 public abstract class BaseEntity {
 
     @CreatedDate
-    @Column(name = "created_at", nullable = false, updatable = false)
+    @Column(name = "created_at", nullable = false, updatable = false, columnDefinition = "TEXT")
     private LocalDateTime createdAt;
 
     @LastModifiedDate
-    @Column(name = "updated_at", nullable = false)
+    @Column(name = "updated_at", nullable = false, columnDefinition = "TEXT")
     private LocalDateTime updatedAt;
 }
